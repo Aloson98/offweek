149a2101def13bd94673f1440070946c0c6525b6
Aloson98 <55749230+Aloson98@users.noreply.github.com>
2022-12-02 04:30:15 -0800

    Add files via upload

--- progress/models.py
+++ progress/models.py
@@ -8,10 +8,11 @@ class news(models.Model):
         ('EDUCATION', 'EDUCATION'),
     )
 
-    title    = models.CharField(max_length=120)
-    author   = models.CharField(max_length=40)
-    category = models.CharField(max_length=80, choices=CATEGORY)
-    content  = models.TextField()
+    title          = models.CharField(max_length=120)
+    author         = models.CharField(max_length=40)
+    category       = models.CharField(max_length=80, choices=CATEGORY)
+    content        = models.TextField()
+    content_image  = models.ImageField(blank=True, upload_to='media/')
 
     def __str__(self):
         return self.title
